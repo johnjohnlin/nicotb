cd4590a2ad05c15fb598c837732011ed4f0726d9
johnjohnlin <johnjohnlin@gmail.com>
2017-07-14 23:34:01 +0800

    Add missing files and update the second example

---
+++ lib/python/nicotb/utils.py
@@ -0,0 +1,144 @@
+# Copyright (C) 2017, Yu Sheng Lin, johnjohnlys@media.ee.ntu.edu.tw
+
+# This file is part of Nicotb.
+
+# Nicotb is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# Nicotb is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with Nicotb.  If not, see <http://www.gnu.org/licenses/>.
+from nicotb import Receiver
+from collections import deque
+from typing import Tuple
+import numpy as np
+
+def DefaultFormator(expect, get):
+	return "Expected:\n{}\nGot:\n{}".format(expect, get)
+
+class Scoreboard(object):
+	scoreboards = list()
+	def __init__(self):
+		self.tests = dict()
+		Scoreboard.scoreboards.append(self)
+
+	def GetTest(self, name, max_err=0, formator=DefaultFormator):
+		if name in self.tests:
+			return self.tests[name]
+		else:
+			ret = Tester(name, max_err, formator)
+			self.tests[name] = ret
+			return ret
+
+	def Report(self):
+		fail = False
+		for t in self.tests.values():
+			if not t.is_clean:
+				print("There are still values in expecting queue of {}\n{}".format(t.name, t.exp))
+				fail = True
+			if t.err != 0:
+				"There are {} errors in {}".format(t.err, t.name)
+				fail = True
+		if fail:
+			print("FAIL")
+		else:
+			print("PASS")
+
+	@classmethod
+	def ReportAll(cls):
+		for scb in cls.scoreboards:
+			scb.Report()
+
+class Tester(object):
+	__slots__ = ["exp", "max_err", "name", "err", "formator"]
+	def __init__(self, name, max_err, formator):
+		self.exp = deque()
+		self.max_err = max_err
+		self.name = name
+		self.err = 0
+		self.formator = formator
+
+	def Get(self, x):
+		assert len(self.exp) != 0, "{} does not expect anything".format(self.name)
+		head = self.exp.pop()
+		if any(not np.array_equal(a,b) for a, b in zip(head, x)):
+			print(self.formator(head, x))
+			assert self.err < self.max_err, "Tester [{}] has reached the max error count".format(self.name)
+			self.err += 1
+
+	def Expect(self, x):
+		self.exp.append(x)
+
+	@property
+	def is_clean(self):
+		return len(self.exp) == 0
+
+class Stacker(Receiver):
+	"""This class stacks a BusWrap object to tuples of ndarray"""
+	__slots__ = ["n", "curn", "buf"]
+	def __init__(self, n=0, callbacks=list()):
+		super(Stacker, self).__init__(callbacks)
+		self.n = n
+		self.curn = 0
+		self.buf = None
+
+	def Resize(self, n, force=False):
+		assert self.is_clean
+		if n == 0:
+			self.buf = None
+		elif self.buf and (self.n < n or force):
+			self.buf = tuple(
+				np.empty(
+					(n,)+a.shape[1:],
+					dtype=a.dtype
+				) for a in self.buf
+			)
+		self.n = n
+
+	def Get(self, x):
+		assert self.n != 0, "Unsized Stacker cannot get values"
+		assert all(not np.any(xval) for xval in x.xs), "Signal has unknown values"
+		if self.buf is None:
+			self.buf = tuple(
+				np.empty(
+					(self.n,)+a.shape,
+					dtype=a.dtype
+				) for a in x.values
+			)
+		for dst, src in zip(self.buf, x.values):
+			dst[self.curn] = src
+		self.curn += 1
+		if self.curn == self.n:
+			super(Stacker, self).Get(tuple(a[:self.curn] for a in self.buf))
+			self.curn = 0
+
+	@property
+	def is_clean(self):
+		return self.curn == 0
+
+class StackerWithX(Receiver):
+	__slots__ = ["n", "curn", "buf"]
+	def __init__(self, n=0):
+		raise NotImplementedError()
+
+	def Resize(self, n, force=False):
+		pass
+
+	def Get(self, x):
+		pass
+
+	@property
+	def is_clean(self):
+		pass
+
+def ExpandSignalGroup(hier: str, prefix: str, groups):
+	return tuple(
+		(None if i != 0 else hier, prefix+s[0], s[1], s[2])
+		for i, s in enumerate(groups)
+	)
--- sim/sum_every3/sm.py
+++ sim/sum_every3/sm.py
@@ -23,9 +23,9 @@ import numpy as np
 N = 10
 
 def main():
-	rs_ev = ToEventIdx("rst")
-	ck_ev = ToEventIdx("clk")
-	odval_ev = ToEventIdx("dst_dval")
+	rs_ev = GetEventIdx("rst")
+	ck_ev = GetEventIdx("clk")
+	odval_ev = GetEventIdx("dst_dval")
 	src_val = GetBus("src_dval")
 	src_dat = GetBus("src")
 	dst_dat = GetBus("dst")
@@ -37,8 +37,8 @@ def main():
 	master = OneWire.Master(src_val, src_dat, ck_ev, callbacks=[print])
 	slave = OneWire.Slave(odval_ev, dst_dat, callbacks=[print, st.Get])
 	values = master.values
-	arr = np.random.randint(16, size=N*3)
-	golden = np.sum(np.reshape(arr, (-1,3)), axis=1, keepdims=1)
+	arr = np.random.randint(16, size=N*3, dtype=np.int32)
+	golden = np.sum(np.reshape(arr, (-1,3)), axis=1, keepdims=1, dtype=np.int32)
 	test1.Expect((golden,)) # must pass
 	# test1.Expect((golden+1,)) # must fail
 	for i in arr:
@@ -46,6 +46,22 @@ def main():
 		yield from master.Send(values)
 		yield ck_ev
 
+CreateBuses({
+	"src_dval": (
+		("", "i_dval", tuple(), None),
+	),
+	"src": (
+		("u_dut", "i", tuple(), None),
+	),
+	"dst": (
+		("u_dut", "o", tuple(), None),
+	),
+})
+CreateEvents({
+	"clk":      ("u_cr.clock",     []),
+	"rst":      ("u_cr.rst_out",   []),
+	"dst_dval": ("u_ldo.detected", [GetBusIdx("dst")]),
+})
 RegisterCoroutines([
 	main(),
 ])
