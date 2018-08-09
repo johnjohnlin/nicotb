01baac1f28874e42e8458a116a620412d89c28fd
johnjohnlin <johnjohnlys@gmail.com>
2018-08-10 00:22:49 +0800

    Add a missing include

--- lib/cpp/nicotb_python.h
+++ lib/cpp/nicotb_python.h
@@ -16,7 +16,6 @@
 // You should have received a copy of the GNU General Public License
 // along with Nicotb.  If not, see <http://www.gnu.org/licenses/>.
 #include <glog/logging.h>
-#include <numpy/ndarraytypes.h>
 #include <Python.h>
 #include <numpy/ndarraytypes.h>
 #include <cstdlib>
